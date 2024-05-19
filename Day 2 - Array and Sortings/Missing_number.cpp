c48bf7377fffd069c9279ef40a6024f944577d92
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-05-19 08:43:04 +0530

    update day 5

---
+++ Day 2 - Array and Sortings/Missing_number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = (n*(n+1))/2;
+
+        for(int i = 0 ; i < n ;i++){
+            sum-=nums[i];
+        }
+
+        return sum;
+    }
+};
