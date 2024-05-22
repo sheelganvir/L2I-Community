34d9947e335540dffcd8045062051d7546df2142
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-05-22 14:26:52 +0530

    update ques of day 3 and day 4

---
+++ Day 3 - Array and Sortings/Contains_Duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        bool check = false;
+        for(int i=1 ; i<nums.size() ; i++){
+            if(nums[i]==nums[i-1]){
+                check = true;
+            }
+        }
+        return check;
+    }
+};
