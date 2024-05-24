792daf69f47643a5fcaefac42123772a798487ac
Ayush Pratap <122193870+ayushpratap27@users.noreply.github.com>
2024-05-24 13:03:38 +0530

    Update Merge_Sorted_Array.cpp

--- Day 4 - Array and Sortings/Merge_Sorted_Array.cpp
+++ Day 4 - Array and Sortings/Merge_Sorted_Array.cpp
@@ -7,3 +7,26 @@ public:
         sort(nums1.begin(), nums1.end());
     }
 };
+
+// Better approach having T.C.=O(m+n) and S.C.=O(1)
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int a = m-1;
+    int b = n-1;
+    int c = m+n-1;
+
+    while(a>=0 && b>=0){
+        if(nums1[a] > nums2[b]){
+            nums1[c--] = nums1[a--];
+        }else{
+            nums1[c--] = nums2[b--];
+        }
+    }
+
+    while(a>=0){
+        nums1[c--]=nums1[a--];
+    }
+
+    while(b >= 0){
+        nums1[c--] = nums2[b--];
+    }
+}
