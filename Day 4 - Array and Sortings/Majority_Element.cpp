f784747fe65d189b997adfe2ca3ed854528637f2
Ayush Pratap <122193870+ayushpratap27@users.noreply.github.com>
2024-05-24 13:07:50 +0530

    Update Majority_Element.cpp

--- Day 4 - Array and Sortings/Majority_Element.cpp
+++ Day 4 - Array and Sortings/Majority_Element.cpp
@@ -6,3 +6,34 @@ public:
         return nums[n/2];
     }
 };
+
+// Optimal Approach having T.C.=O(n)
+// This approach is known as Moore Voting Algorithm
+int majorityElement(vector<int> v) {
+
+    int n = v.size();
+    int cnt=0;
+    int ele;
+
+    for(int i=0;i<n;i++){
+        if(cnt==0){
+            ele=v[i];
+            cnt=1;
+        }else if(v[i]==ele){
+            cnt++;
+        }else{
+            cnt--;
+        }
+    }
+    
+    int cnt1=0;
+    for(int i=0;i<n;i++){
+        if(v[i]==ele){
+            cnt1++;
+        }
+    }
+    if(cnt1>(n/2)){
+        return ele;
+    }
+    return -1;
+}
