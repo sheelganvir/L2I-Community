9e7df6ee2d43fffa7756c15b7ae22a06aecc7f66
Ayush Pratap <122193870+ayushpratap27@users.noreply.github.com>
2024-05-24 13:22:31 +0530

    Update Set_Matrix_Zeroes.cpp

--- Day 3 - Array and Sortings/Set_Matrix_Zeroes.cpp
+++ Day 3 - Array and Sortings/Set_Matrix_Zeroes.cpp
@@ -23,4 +23,47 @@ public:
             }
         }
     }
+
+//   T.C.=O(n*m*(n+m))       S.C.=o(n*m)
 };
+
+void setZeroes(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    int col0=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]==0){
+                matrix[i][0]=0;
+                if(j!=0){
+                    matrix[0][j]=0;
+                }else{
+                     col0=0;
+                 }
+             }
+         }
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+            if(matrix[i][j]!=0){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+    if(matrix[0][0]==0){
+        for(int j=0;j<m;j++){
+            matrix[0][j]=0;
+        }
+    }
+    if(col0==0){
+        for(int i=0;i<n;i++){
+            matrix[i][0]=0;
+        }
+    }
+        
+} 
+//    T.C.=O(n*m)   S.C.=O(1)
