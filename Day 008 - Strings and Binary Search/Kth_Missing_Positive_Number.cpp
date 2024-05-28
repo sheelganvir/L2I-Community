efc4478acae57e1b8175fef97959aafd113c488a
Ayush Pratap <122193870+ayushpratap27@users.noreply.github.com>
2024-05-28 15:20:25 +0530

    Update Kth_Missing_Positive_Number.cpp

--- Day 008 - Strings and Binary Search/Kth_Missing_Positive_Number.cpp
+++ Day 008 - Strings and Binary Search/Kth_Missing_Positive_Number.cpp
@@ -12,3 +12,23 @@ int findKthPositive(vector<int>& arr, int k) {
     return k;
 }
 //  T.C.= O(n)  S.C.= O(1)
+
+//************* Optimised Approach ************
+int findKthPositive(vector<int>& arr, int k) {
+        int low = 0;
+        int high = arr.size()-1;
+
+        while(low <= high){
+            int mid = (low+high)/2;
+            int missing = arr[mid] - (mid+1);
+
+            if(missing < k){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+
+        return high+1+k;
+}
+//  T.C.= O(logn)   S.C.= O(1)
