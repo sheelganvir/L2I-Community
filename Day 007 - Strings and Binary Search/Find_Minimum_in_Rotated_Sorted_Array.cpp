c95f9c01e0d1ef900112991709bc55b42b6d0b4e
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-05-31 07:41:52 +0530

    update day 15

--- Day 007 - Strings and Binary Search/Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ Day 007 - Strings and Binary Search/Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -16,3 +16,27 @@ int findMin(vector<int>& arr) {
         
 }
 //  T.C.= O(logn)  S.C.= O(1)
+
+Alternate Code
+
+
+int findMin(vector<int>& nums) {
+    if(nums.size()==1) return nums[0];
+    int lo=0;
+    int hi = nums.size()-1;
+    if(nums[lo]<nums[hi]){
+        return nums[lo];
+    }
+    while(lo<=hi){
+        int mid = (lo+hi)/2;
+        if(nums[mid]>nums[mid+1]) return nums[mid+1];
+        if(nums[mid]<nums[mid-1]) return  nums[mid];
+        if(nums[mid]>nums[lo]){
+            lo=mid+1;
+        }
+        else{
+            hi=mid-1;
+        }
+    }
+    return -1;
+}
